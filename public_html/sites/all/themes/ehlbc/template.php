c1c9ea3b5b7fd0c46470355b09b26eedc261e067
Colin <colin@raisedeyebrow.com>
2013-08-21 13:20:47 -0700

    Tidy template.php

--- public_html/sites/all/themes/ehlbc/template.php
+++ public_html/sites/all/themes/ehlbc/template.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Put yout custom function-y goodness here
@@ -7,8 +8,12 @@
  * Cogito_child theme theme.
  */
 
+
+/**
+ * Implements template_preprocess_page().
+ */
 function ehlbc_preprocess_page(&$variables) {
   drupal_add_js('http://w.sharethis.com/button/buttons.js', 'external');
   drupal_add_js('stLight.options({publisher:"4e1667cf-8007-473e-96b5-3eda0d97f1fb"});', 'inline');
-}
+} // ehlbc_preprocess_page()
 
