e6e8e02b68f2ac0fa4ed0fa53b99540e40b54cfe
Christopher Torgalson <manager@bedlamhotel.com>
2013-08-30 10:56:42 -0700

    Add panels layout

---
+++ public_html/sites/all/themes/ehlbc/layouts/ehlbc_dashboard/panels-ehlbc-dashboard.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @file
+ * Template for a 2 column panel layout.
+ *
+ * This template provides a two column 33%-66% panel display layout.
+ *
+ * Variables:
+ * - $id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ *   panel of the layout. This layout supports the following sections:
+ *   - $content['sidebar_left']: Content in the left sidebar.
+ *   - $content['content_main']: Content in the main content area.
+ *
+ * We've left the basic layout, regardless if there is content to fill it!
+ */
+?>
+<div class="ehlbc_dashboard"<?php if (!empty($css_id)) { print ' id="$css_id"'; } ?>>
+  <div class="row">
+    <div class="columns four" id="sidebar-left">
+      <?php if ($content['sidebar_left']): ?>
+        <?php print $content['sidebar_left']; ?>
+      <?php endif; ?>
+    </div>
+    <div class="columns eight" id="content-main">
+      <?php if ($content['content_main']): ?>
+        <?php print $content['content_main']; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
+
