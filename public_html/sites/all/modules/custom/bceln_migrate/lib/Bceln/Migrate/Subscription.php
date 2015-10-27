fa63e6e6257c7d43e98d7a97ec4e0ec9a6870720
Raphael Huefner <raphael@affinitybridge.com>
2015-10-27 15:34:05 -0700

    #7 add The "PHP League CSV" library plus wrapper Drupal module and use it to import IP addresses and subscriptions

--- public_html/sites/all/modules/custom/bceln_migrate/lib/Bceln/Migrate/Subscription.php
+++ public_html/sites/all/modules/custom/bceln_migrate/lib/Bceln/Migrate/Subscription.php
@@ -1,7 +1,5 @@
 <?php
 
-use League\Csv\Reader as Reader;
-
 class Bceln_Migrate_Subscription {
   static protected $csvFileName = 'subscriptions.csv';
 
@@ -9,27 +7,23 @@ class Bceln_Migrate_Subscription {
   static protected $subscriptions = [];
 
   static protected function loadSubscriptions() {
-    static::$isLoaded = TRUE;
-    if (league_csv_library_load()) {
-      $fullFileName = Bceln_Migrate_Config::getFullCsvFileName(static::$csvFileName);
-      $data = league_csv_load_with_header($fullFileName);
-      foreach ($data as $row) {
-        $db_id = $row['db_id'];
-        $inst_id = $row['inst_id'];
-        static::$subscriptions[$db_id][$inst_id] = $inst_id;
+    if (! static::$isLoaded) {
+      static::$isLoaded = TRUE;
+      if (league_csv_library_load()) {
+        $fullFileName = Bceln_Migrate_Config::getFullCsvFileName(static::$csvFileName);
+        $data = league_csv_load_with_header($fullFileName);
+        foreach ($data as $row) {
+          $db_id = $row['db_id'];
+          $inst_id = $row['inst_id'];
+          static::$subscriptions[$db_id][$inst_id] = $inst_id;
+        }
       }
     }
   }
 
-  static public function getAllSubscriptions() {
-    static::loadSubscriptions();
-    return static::$subscriptions;
-  }
-
   static public function getSubscriptionsByDbId($db_id) {
-    if (! static::$isLoaded) {
-      static::loadSubscriptions();
-    }
+    static::loadSubscriptions();
+
     $db_id = trim($db_id);
     if (isset(static::$subscriptions[$db_id])) {
       return array_values(static::$subscriptions[$db_id]);
