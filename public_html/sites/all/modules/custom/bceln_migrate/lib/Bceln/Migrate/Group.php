dc7355ff829423e80e2e60ea72611b0b51796190
Raphael Huefner <raphael@affinitybridge.com>
2015-11-03 18:06:19 -0800

    #31 Import Contact Groups

--- public_html/sites/all/modules/custom/bceln_migrate/lib/Bceln/Migrate/Group.php
+++ public_html/sites/all/modules/custom/bceln_migrate/lib/Bceln/Migrate/Group.php
@@ -5,25 +5,25 @@ class Bceln_Migrate_Group {
   static protected $relationCsvFileName = 'contacts_groups.csv';
 
   static protected $isLoaded = FALSE;
-  static protected $groups = [];
   static protected $groupNamesByContactId = [];
 
   static protected function loadGroups() {
     if (! static::$isLoaded) {
       static::$isLoaded = TRUE;
       if (league_csv_library_load()) {
+        $groups = [];
         $groupData = league_csv_load_with_header(Bceln_Migrate_Config::getFullCsvFileName(static::$groupCsvFileName));
         $relationData = league_csv_load_with_header(Bceln_Migrate_Config::getFullCsvFileName(static::$relationCsvFileName));
         foreach ($groupData as $row) {
-          $group_id = $row['group_id'];
-          $group_name = $row['group_name'];
-          static::$groups[$group_id] = $group_name;
+          $group_id = trim($row['group_id']);
+          $group_name = trim($row['group_name']);
+          $groups[$group_id] = $group_name;
         }
         foreach ($relationData as $row) {
-          $contact_id = $row['contact_id'];
-          $group_id = $row['group_id'];
-          if (isset(static::$groups[$group_id])) {
-            $group_name = static::$groups[$group_id];
+          $contact_id = trim($row['contact_id']);
+          $group_id = trim($row['group_id']);
+          if (isset($groups[$group_id])) {
+            $group_name = $groups[$group_id];
             static::$groupNamesByContactId[$contact_id][$group_name] = $group_name;
           }
         }
